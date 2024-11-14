dcb6a344209a462a737cc756de2c595569d59b1d
Deshna Doshi <67083489+deshnadoshi@users.noreply.github.com>
2024-11-14 15:40:33 -0500

    Update SQL File

--- create_db.sql
+++ create_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `railwaybooking` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `railwaybooking`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: railwaybooking
@@ -80,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-14 15:34:51
+-- Dump completed on 2024-11-14 15:39:42
