fe3f2a888f558722cd0092a853d821592f096e41
palak-singh73 <145735438+palak-singh73@users.noreply.github.com>
2024-11-18 14:36:45 -0500

    Create SQL Dump

--- create_db.sql
+++ create_db.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `railwaybooking` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `railwaybooking`;
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: railwaybooking
 -- ------------------------------------------------------
--- Server version	8.0.36
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -41,7 +41,7 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-INSERT INTO `customers` VALUES ('dd1035','trains123','Deshna','Doshi','dd1035@rutgers.edu'),('ps1173','trains456','Palak','Singh','ps1173@rutgers.edu');
+INSERT INTO `customers` VALUES ('dd1035','trains123','Deshna','Doshi','dd1035@rutgers.edu'),('hihello','hellohi','Hi','Hello','hihello@gmail.com'),('hrithik','husband!','Hrithik','Roshan','hrithik@gmail.com'),('ps1173','trains456','Palak','Singh','ps1173@rutgers.edu');
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -69,8 +69,129 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (12345678,'Jasmine','Hanjra','jasmine','pword567'),(123456789,'Maria','Jaral','maria','pword123');
+INSERT INTO `employees` VALUES (12345678,'Jasmine','Hanjra','jasmine','pwrod567'),(123456789,'Maria','Jaral','maria','pword123');
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `res_number` int NOT NULL,
+  `res_datetime` datetime DEFAULT NULL,
+  `total_fare` float DEFAULT NULL,
+  `is_roundtrip` tinyint(1) DEFAULT NULL,
+  `discount_type` varchar(1) DEFAULT NULL,
+  `res_origin_station_id` int DEFAULT NULL,
+  `res_destination_station_id` int DEFAULT NULL,
+  `line_name` varchar(50) DEFAULT NULL,
+  `portfolio_username` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`res_number`),
+  KEY `res_origin_station_id` (`res_origin_station_id`),
+  KEY `res_destination_station_id` (`res_destination_station_id`),
+  KEY `line_name` (`line_name`),
+  KEY `portfolio_username` (`portfolio_username`),
+  CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`res_origin_station_id`) REFERENCES `stations` (`station_id`),
+  CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`res_destination_station_id`) REFERENCES `stations` (`station_id`),
+  CONSTRAINT `reservations_ibfk_3` FOREIGN KEY (`line_name`) REFERENCES `trainschedules` (`line_name`),
+  CONSTRAINT `reservations_ibfk_4` FOREIGN KEY (`portfolio_username`) REFERENCES `customers` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stations`
+--
+
+DROP TABLE IF EXISTS `stations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stations` (
+  `station_id` int NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `state` char(2) DEFAULT NULL,
+  `city` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`station_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stations`
+--
+
+LOCK TABLES `stations` WRITE;
+/*!40000 ALTER TABLE `stations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trains`
+--
+
+DROP TABLE IF EXISTS `trains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trains` (
+  `train_id` int NOT NULL,
+  `line_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`train_id`),
+  KEY `line_name` (`line_name`),
+  CONSTRAINT `trains_ibfk_1` FOREIGN KEY (`line_name`) REFERENCES `trainschedules` (`line_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trains`
+--
+
+LOCK TABLES `trains` WRITE;
+/*!40000 ALTER TABLE `trains` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trains` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trainschedules`
+--
+
+DROP TABLE IF EXISTS `trainschedules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trainschedules` (
+  `line_name` varchar(50) NOT NULL,
+  `num_stops` int DEFAULT NULL,
+  `fare` int DEFAULT NULL,
+  `train_arrival_time` datetime DEFAULT NULL,
+  `train_departure_time` datetime DEFAULT NULL,
+  `travel_time` int DEFAULT NULL,
+  `origin_stop_station_id` int DEFAULT NULL,
+  `destination_stop_station_id` int DEFAULT NULL,
+  PRIMARY KEY (`line_name`),
+  KEY `origin_stop_station_id` (`origin_stop_station_id`),
+  KEY `destination_stop_station_id` (`destination_stop_station_id`),
+  CONSTRAINT `trainschedules_ibfk_1` FOREIGN KEY (`origin_stop_station_id`) REFERENCES `stations` (`station_id`),
+  CONSTRAINT `trainschedules_ibfk_2` FOREIGN KEY (`destination_stop_station_id`) REFERENCES `stations` (`station_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trainschedules`
+--
+
+LOCK TABLES `trainschedules` WRITE;
+/*!40000 ALTER TABLE `trainschedules` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trainschedules` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -82,4 +203,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-14 15:39:42
+-- Dump completed on 2024-11-18 14:34:49
